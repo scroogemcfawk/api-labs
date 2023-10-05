77a3568376ddedee8ac5f19cc967526a6c2cb23f
scroogemcfawk <scroogemcfawk@gmail.com>
2023-10-05 10:06:10 +0700

    1 lab clean-up

--- src/main/kotlin/com/scroogemcfawk/tsu/apilabs/server/repository/UserRepo.kt
+++
@@ -1,8 +0,0 @@
-package com.scroogemcfawk.tsu.apilabs.server.repository
-
-import com.scroogemcfawk.tsu.apilabs.server.entity.UserEntity
-import org.springframework.data.repository.CrudRepository
-
-interface UserRepo: CrudRepository<UserEntity, Long> {
-    fun findByUsername(username: String): UserEntity?
-}
